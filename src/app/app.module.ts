2bcc87045c08133a86fbd3a0efc282604e836bde
GlaucoTolotto <glaucotolotto@gmail.com>
2025-07-03 21:54:15 -0300

    adiciona modulo dos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
 
-import { PoModule } from '@po-ui/ng-components';
-import { PoTemplatesModule } from '@po-ui/ng-templates';
-import { PoCodeEditorModule } from '@po-ui/ng-code-editor';
-import { PoStorageModule } from '@po-ui/ng-storage';
-import { PoSyncModule } from '@po-ui/ng-sync';
+import { PoModule, PoFieldModule } from "@po-ui/ng-components";
+import { PoTemplatesModule } from "@po-ui/ng-templates";
+import { PoCodeEditorModule } from "@po-ui/ng-code-editor";
+import { PoStorageModule } from "@po-ui/ng-storage";
+import { PoSyncModule } from "@po-ui/ng-sync";
 
-import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from "./app.component";
+import { HttpClientModule } from "@angular/common/http";
+
 
 @NgModule({
   imports: [
@@ -21,8 +22,8 @@ import { HttpClientModule } from '@angular/common/http';
     FormsModule,
     RouterModule.forRoot([]),
     HttpClientModule,
-
     PoModule,
+    PoFieldModule,
     PoTemplatesModule,
     PoCodeEditorModule,
     PoStorageModule.forRoot(),
