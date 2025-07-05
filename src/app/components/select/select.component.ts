ccb2c38c67a4705adc0ab767c0fb9bc908c8113c
GlaucoTolotto <glaucotolotto@gmail.com>
2025-07-05 18:18:52 -0300

    ajusta componente para aceitar NgModel e ReactiveForms

--- src/app/components/select/select.component.ts
+++ src/app/components/select/select.component.ts
@@ -1,18 +1,43 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, forwardRef } from "@angular/core";
 import { Option } from "src/app/models/option.model";
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: "app-select",
   templateUrl: "./select.component.html",
   styleUrls: ["./select.component.css"],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SelectComponent),
+      multi: true
+    }
+  ]
 })
-export class SelectComponent {
+export class SelectComponent implements ControlValueAccessor {
   @Input() disabled: boolean;
   @Input() options: Option[] = [];
   @Input() placeholder: string;
   selectedOption: string = '';
 
-   onSelectionChange(): void {
-    console.log("Selected option:", this.selectedOption);
-   }
+  onChange = (_: any) => {};
+  onTouched = () => {};
+
+  writeValue(val: string): void {
+    this.selectedOption = val;
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  onSelectionChange(): void {
+    this.onChange(this.selectedOption);
+    this.onTouched();
+  }
 }
