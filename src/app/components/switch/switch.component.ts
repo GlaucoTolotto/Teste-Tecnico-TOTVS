04a2ceb4b87efe4d5e26dadd9958a57ab5dab149
GlaucoTolotto <glaucotolotto@gmail.com>
2025-07-05 16:46:39 -0300

    finaliza estilo do switch

--- src/app/components/switch/switch.component.ts
+++ src/app/components/switch/switch.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, forwardRef } from "@angular/core";
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
-  selector: 'app-switch',
-  templateUrl: './switch.component.html',
-  styleUrls: ['./switch.component.css']
+  selector: "app-switch",
+  templateUrl: "./switch.component.html",
+  styleUrls: ["./switch.component.css"],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SwitchComponent),
+      multi: true
+    }
+  ]
 })
-export class SwitchComponent {
+export class SwitchComponent implements ControlValueAccessor {
+  @Input()
+  set value(val: boolean) {
+    this._value = val;
+    this.onChange(val);
+    this.valueChange.emit(this._value);
+  }
+  get value(): boolean {
+    return this._value;
+  }
+  private _value: boolean = false;
 
+  @Output() valueChange: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  onChange = (_: any) => {};
+  onTouched = () => {};
+
+  writeValue(val: boolean): void {
+    this._value = val;
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  toggle(): void {
+    this.value = !this.value;
+    this.onTouched();
+  }
 }
